74f03b944282488b8acc63fb8a7606ca165c23f5
imran-rassi <imran.rassi.2020@gmail.com>
2022-11-21 11:42:42 -0500

    added the employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,9 +8,12 @@ VALUES
     ("Marketing"),
     ("Engineering")
 
--- Creates the following rows in the 'role' table --
+-- Creating the rows 
+-- in the role tablt --
+
 INSERT INTO role (title, salary, department_id)
 VALUES
+
 ("Sales Manager", "150000.00", 1),
 ("Salesperson", "75000.00", 1),
 ("Account Manager", "145000.00", 2),
@@ -20,3 +23,25 @@ VALUES
 ("Engineer Lead Manager", "165000.00", 4),
 ("Software Engineer", "130000.00", 4),
 
+-- Creating the rows
+-- in the employee table --
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+
+("Ali", "DJ", 1, NULL),
+("Micheal", "CJ", 2, 1),
+("Frank", "Adams", 2, 1),
+("Maria", "Rodriguez", 3, NULL),
+("Killy", "Dobsom", 4, 4),
+("Messi", "Octa", 4, 4),
+("Tylor", "Ale", 5, NULL),
+("Robin", "Bauer", 6, 7),
+("Salim", "India", 6, 7),
+
+-- Selects all columns from
+-- each table in database --
+
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM department;
