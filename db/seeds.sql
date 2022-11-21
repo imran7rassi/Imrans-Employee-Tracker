4f05d96c12b952a3a5451b723fcf60039ba75bd9
imran-rassi <imran.rassi.2020@gmail.com>
2022-11-21 11:31:52 -0500

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+
+-- inserting the name of departments --
+
+INSERT INTO department (name)
+VALUES
+    ("Sales"),
+    ("Finance"),
+    ("Marketing"),
+    ("Engineering")
+
+-- Creates the following rows in the 'role' table --
+INSERT INTO role (title, salary, department_id)
+VALUES
+("Sales Manager", "150000.00", 1),
+("Salesperson", "75000.00", 1),
+("Account Manager", "145000.00", 2),
+("Accountant", "133000.00", 2);
+("Marketing lead", "220000.00", 3),
+("Product Marketing Manager", "175000.00", 3),
+("Engineer Lead Manager", "165000.00", 4),
+("Software Engineer", "130000.00", 4),
+
