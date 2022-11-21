c1cd24022cf090b95c09172ef88de8eafcb9cf91
imran-rassi <imran.rassi.2020@gmail.com>
2022-11-20 23:06:30 -0500

    added schema codes

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,43 @@
 
+-- Droping the employee tracker database if
+-- it's already exist
 DROP DATABASE IF EXISTS employeeTracker;
--- Creates a database called employeeTracker --
+
+-- we create a database by name of
+-- employee tracker 
 CREATE DATABASE employeeTracker;
+
+-- use the database --
 USE employeeTracker;
+
+-- Create the table for department --
+
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- create the table for roles --
+
+CREATE TABLE roles(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- Create the table for employee --
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
